f010d96d80720219c314da9d08fbb65d804e0969
roxywasiak <ruksclone@hotmail.com>
2022-06-11 21:37:07 +0100

    added the questions for user to make team and the manager questions object arrays

--- src/utils/questions.js
+++ src/utils/questions.js
@@ -1,50 +1,60 @@
 // build the team name
 const teamQuestions = [
-    {
-
-    },
+  {
+    type: "input",
+    message: "Enter a team name:",
+    name: " teamName",
+  },
 ];
 
-// manager questions
+// manager questions name id email office number
 const managerQuestions = [
-    {
-
-    },
+  {
+    type: "input",
+    message: " Enter your name",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "Enter your id",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "Enter your email",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "Enter your office number",
+    name: "officeNumber",
+  },
 ];
 
 //employee role
 const employeeRoleQuestions = [
-    {
-
-    },
+  {
+    type: "list",
+    message: "Select employee role",
+    name: "employeeRole",
+    choices: ["Engineer", "Intern"],
+  },
 ];
 
 // engineer questions
-const engineerQuestions = [
-    {
-
-    };
-];
+const engineerQuestions = [{}];
 
 //intern questions
-const internQuestions = [
-    {
-
-    },
-];
+const internQuestions = [{}];
 
 //continue questions/confirm to add another etc
-const questionsContinue = [
-    {
-
-    },
-]
+const questionsContinue = [{}];
 
 //export the objects
 module.exports = {
-    teamQuestions,
-    managerQuestions,
-    engineerQuestions,
-    internQuestions,
-    questionsContinue,
+  teamQuestions,
+  managerQuestions,
+  engineerQuestions,
+  internQuestions,
+  questionsContinue,
 };
