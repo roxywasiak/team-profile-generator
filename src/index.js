c2463034fca1f2d302d22862a5134587a446a99f
roxywasiak <ruksclone@hotmail.com>
2022-06-11 20:25:26 +0100

    added the questions object array and imported them as questions in index js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 //install the dependencies
+const inquirer = require ("inquirer");
+
 
 //import the questions
 
-//import the functions you want:
+//import the object as require you want:
+const {
+    teamQuestions,
+    managerQuestions,
+    engineerQuestions,
+    internQuestions,
+    questionsContinue,
+} = require("questions.js");
+
 //write to file function
+const {writeToFile} = require("./utils/writetofile");
 //generate html cards file
 
 //import classes
 const { Manager } = require("./lib/Manager");
 const { Engineer } = require("./lib/Engineer");
 const { Intern } = require("./lib/Engineer");
+
+
+//init function 
--- src/utils/questions.js
+++ src/utils/questions.js
@@ -1,13 +1,50 @@
 // build the team name
+const teamQuestions = [
+    {
+
+    },
+];
 
 // manager questions
+const managerQuestions = [
+    {
 
-//employee types questions
+    },
+];
+
+//employee role
+const employeeRoleQuestions = [
+    {
+
+    },
+];
 
 // engineer questions
+const engineerQuestions = [
+    {
+
+    };
+];
 
 //intern questions
+const internQuestions = [
+    {
+
+    },
+];
 
 //continue questions/confirm to add another etc
+const questionsContinue = [
+    {
+
+    },
+]
 
 //export the objects
+module.exports = {
+    teamQuestions,
+    managerQuestions,
+    engineerQuestions,
+    internQuestions,
+    questionsContinue,
+};
